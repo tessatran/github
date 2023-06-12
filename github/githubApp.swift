c26a212b754c155f54069e7d286138465c4c7cdd
Tessa Tran <testra913@gmail.com>
2023-06-12 08:03:01 -0700

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Tessa Tran on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
